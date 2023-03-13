feacc7fee2b7ad7572abea3870c089cb1c1ea702
Charles Ackley <charles.ackley@omnifederal.com>
2023-03-13 02:38:57 -0400

    Milestone 3, CST150.

--- Milestone Test/UnitTest1.cs
+++ Milestone Test/UnitTest1.cs
@@ -13,19 +13,19 @@ namespace Milestone_Test
         public void SearchTitleTest()
         {
             Inventory testInventory = new Inventory();
-            ArrayList inventory = testInventory.populateInventory();
+            Array inventory = testInventory.populateInventory().ToArray();
             
             //Should return only one item from the inventory.
-            var results = testInventory.search("Title", inventory, "Book1").ToArray();
+            var results = testInventory.search("Title", inventory, "Book1", true);
             foreach(Inventory.inventoryStruct result in results)
             {
                 Assert.IsTrue(result.title.Equals("Book1"));
             }
 
-            //Should return 3 items from the inventory
-            results = testInventory.search("Title", inventory, "Book").ToArray();
+            //Should return 5 items from the inventory
+            results = testInventory.search("Title", inventory, "Book", true);
             int count = 1;
-            Assert.IsTrue(results.Length == 3);
+            Assert.IsTrue(results.Length == 4);
             foreach (Inventory.inventoryStruct result in results)
             {
                 Assert.IsTrue(result.title.Equals("Book" + count));
@@ -38,20 +38,48 @@ namespace Milestone_Test
         public void SearchGenereTest()
         {
             Inventory testInventory = new Inventory();
-            ArrayList inventory = testInventory.populateInventory();
+            Array inventory = testInventory.populateInventory().ToArray();
 
             //Each should return one inventory item 
-            var results = testInventory.search("Genere", inventory, "Romance").ToArray();
+            var results = testInventory.search("Genere", inventory, "Romance", true);
             foreach (Inventory.inventoryStruct result in results)
             {
                 Assert.IsTrue(result.title.Equals("Book3"));
                 Assert.IsTrue(Array.Find(result.genere, element => element.Contains("Romance")) != null);
             }
-            results = testInventory.search("Genere", inventory, "Adventure").ToArray();
+            results = testInventory.search("Genere", inventory, "Adventure", true);
             foreach (Inventory.inventoryStruct result in results)
             {
                 Assert.IsTrue(result.title.Equals("Book2"));
                 Assert.IsTrue(Array.Find(result.genere, element => element.Contains("Adventure")) != null);
+            }
+        }
+
+        [TestMethod]
+        //Test that user can search by instock items
+        public void SearchInstockTest()
+        {
+            Inventory testInventory = new Inventory();
+            Array inventory = testInventory.populateInventory().ToArray();
+
+            //Should return 4 items from the inventory.
+            var results = testInventory.search(inventory, true);
+            Assert.IsTrue(results.Length == 4);
+            int count = 1;
+            foreach (Inventory.inventoryStruct result in results)
+            {
+                Assert.IsTrue(result.title.Equals("Book" + count));
+                count++;
+            }
+
+            //Should return 5 items from the inventory
+            results = testInventory.search(inventory, false);
+            count = 1;
+            Assert.IsTrue(results.Length == 5);
+            foreach (Inventory.inventoryStruct result in results)
+            {
+                Assert.IsTrue(result.title.Equals("Book" + count));
+                count++;
             }
         }
     }
